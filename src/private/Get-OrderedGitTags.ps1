3c74a5c985a0b83661c48a46a02f3be0c2fd3b10
David Wallis <davidwallis3101@users.noreply.github.com>
2024-04-08 15:18:04 +0100

    Update Get-OrderedGitTags.ps1
    fix: :bug: Fix issue using -match as this returns true, rather than the tags

--- src/private/Get-OrderedGitTags.ps1
+++ src/private/Get-OrderedGitTags.ps1
@@ -15,7 +15,7 @@ function Get-OrderedGitTags {
     )
 
     process {
-        $Tags = ((git show-ref --tags) -match 'refs/tags') -replace '.+refs/tags/'
+        $Tags = ((git show-ref --tags)  | where {$_ -match 'refs/tags'}) -replace '.+refs/tags/'
 
         $Tags | Sort-Object { [system.version]($_ -creplace $VersionPattern)} -Descending:$($PSCmdlet.ParameterSetName -eq "desc")
     }
